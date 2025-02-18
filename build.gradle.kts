e1d3c8269e3eb7560489e36f346a1504cefc51ed
Michal Harakal <michal.harakal@googlemail.com>
2025-02-18 22:08:33 +0100

    Add initial version of antora generated static web page

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.gradleVersionsPlugin) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+}
