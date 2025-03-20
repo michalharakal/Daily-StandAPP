396ebf1465f321b5cd9697f8cf5bd2e7b9435d51
Michal <michal.harakal@googlemail.de>
2025-03-20 23:23:05 +0100

    Add common test skeleton to data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -28,58 +28,63 @@ kotlin {
         // (Optional) Use JDK 17 toolchain for compilation
         // java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
     }
-        listOf(
-            iosArm64(),
-            iosSimulatorArm64()
-        ).forEach { iosTarget ->
-            iosTarget.binaries.framework {
-                baseName = "SinusApproximatorKit"
-                isStatic = true
-            }
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "SinusApproximatorKit"
+            isStatic = true
         }
+    }
 
 
-        @OptIn(ExperimentalWasmDsl::class)
-        wasmJs {
-            browser {
-                val rootDirPath = project.rootDir.path
-                val projectDirPath = project.projectDir.path
-                commonWebpackConfig {
-                    devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                        static = (static ?: mutableListOf()).apply {
-                            // Serve sources to debug inside browser
-                            add(rootDirPath)
-                            add(projectDirPath)
-                        }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser {
+            val rootDirPath = project.rootDir.path
+            val projectDirPath = project.projectDir.path
+            commonWebpackConfig {
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(rootDirPath)
+                        add(projectDirPath)
                     }
                 }
             }
         }
-
-        sourceSets {
-            commonMain.dependencies {
-                implementation(libs.kotlinx.io.core)
-                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
-            }
-            jvmMain.dependencies {
-                implementation("org.eclipse.jgit:org.eclipse.jgit:7.1.0.202411261347-r")
-                implementation("org.slf4j:slf4j-simple:2.0.16")
-            }
-            jvmTest.dependencies {
-                implementation("org.junit.jupiter:junit-jupiter-api:5.12.1")
-            }
-        }
     }
 
-    android {
-        namespace = "sk.ai.net.client.shared"
-        compileSdk = libs.versions.android.compileSdk.get().toInt()
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.io.core)
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
         }
-        defaultConfig {
-            minSdk = libs.versions.android.minSdk.get().toInt()
+        commonTest {
+            dependencies {
+                api(kotlin("test"))
+            }
+        }
+        jvmMain.dependencies {
+            implementation("org.eclipse.jgit:org.eclipse.jgit:7.1.0.202411261347-r")
+            implementation("org.slf4j:slf4j-simple:2.0.16")
+        }
+        jvmTest.dependencies {
+            implementation("org.junit.jupiter:junit-jupiter-api:5.12.1")
         }
     }
+}
+
+android {
+    namespace = "sk.ai.net.client.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
 
