f9b8d260812dcf040a8714c2e51c959ab1817732
Falk Sippach <falk@jug-da.de>
2025-03-20 13:40:06 +0100

    added author to commits query

--- data/src/commonMain/kotlin/de/jug_da/data/git/GitClient.kt
+++ data/src/commonMain/kotlin/de/jug_da/data/git/GitClient.kt
@@ -2,4 +2,4 @@ package de.jug_da.data.git
 
 import kotlinx.datetime.Instant
 
-expect fun commitsByPeriod(start: Instant, end: Instant): List<GitInfo>
+expect fun commitsByAuthorAndPeriod(author: String, start: Instant, end: Instant): List<GitInfo>
--- data/src/wasmJsMain/kotlin/de/jug_da/data/git/GitExtractor.wasmJs.kt
+++ data/src/wasmJsMain/kotlin/de/jug_da/data/git/GitExtractor.wasmJs.kt
@@ -2,6 +2,6 @@ package de.jug_da.data.git
 
 import kotlinx.datetime.Instant
 
-actual fun commitsByPeriod(start: Instant, end: Instant): List<GitInfo> {
+actual fun commitsByAuthorAndPeriod(author: String, start: Instant, end: Instant): List<GitInfo> {
     TODO("Not yet implemented")
 }
