2ea9640ff79cd68bf32b7d8fcab236f899b6fb92
Michal Harakal <michal.harakal@googlemail.com>
2025-03-23 19:26:31 +0100

    Add Java 21 as build requirement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,3 +31,11 @@ dependencyResolutionManagement {
 
 include(":composeApp", ":shared", ":data", ":domain", ":llm")
 include("StandAPP-cli")
+
+check(JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_21)) {
+    """
+    Impulsee core requires JDK 21+ but it is currently using JDK ${JavaVersion.current()}.
+    Java Home: [${System.getProperty("java.home")}]
+    https://developer.android.com/build/jdks#jdk-config-in-studio
+    """.trimIndent()
+}
