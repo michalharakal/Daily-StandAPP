da72864904aab637a810d508e84c8b9a4a6f19dc
Michal Harakal <michal.harakal@googlemail.com>
2026-01-27 11:33:58 +0100

    clean fiels

--- llm/src/commonMain/kotlin/de/jug_da/standapp/llm/Summarize.kt
+++
@@ -1,9 +0,0 @@
-package de.jug_da.standapp.llm
-
-interface LLMSummarizer {
-    suspend fun summarize(text: String): String
-    fun summarize(text: String, callback: (String) -> Unit): String
-}
-
-expect fun getLLMSummarizer(): LLMSummarizer
-
